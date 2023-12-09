9edd6bdaf2f282037ec36e588c56578eada647bf
Daniel URSU-BALACI <dursu@bitdefender.biz>
2023-12-09 12:50:39 +0200

    Added article functions

---
+++ src/main/java/com/example/demo/controller/ArticolController.java
@@ -0,0 +1,86 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ArticolDTO;
+import com.example.demo.dto.ResponseDto;
+import com.example.demo.model.Articol;
+import com.example.demo.service.ArticolService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+public class ArticolController {
+	
+	public final ArticolService articolService;
+	
+	public ArticolController(ArticolService articolService) {
+		this.articolService = articolService;
+	}
+	
+	@GetMapping("/all")
+	public ResponseEntity<ArticolDTO> getAllArticles() {
+		List<Articol> articles = this.articolService.getAllArticles();
+		return ResponseEntity.ok(new ArticolDTO(true, articles));
+	}
+	
+	@GetMapping("/{title}")
+	public ResponseEntity<ArticolDTO> getArticle(@PathVariable String title) {
+		List<Articol> articles = this.articolService.getAllArticles();
+		Optional<Articol> result = articles.stream().filter(article -> article.getTitlu().equals(title)).findFirst();
+		if (result.isEmpty()) {
+			return ResponseEntity.ok(new ArticolDTO(false, result));
+		}
+		return ResponseEntity.ok(new ArticolDTO(true, result));
+	}
+	@GetMapping("/alltitles/{title}")
+	public ResponseEntity<ArticolDTO> getAllTitles(@PathVariable String title) {
+		List<Articol> articles = this.articolService.getAllArticles();
+		List<String> result = articles.stream()
+						.map(Articol::getTitlu)
+						.filter(titlu -> titlu.contains(title))
+						.collect(Collectors.toList());
+		if (result.isEmpty()) {
+			return ResponseEntity.ok(new ArticolDTO(false, result));
+		}
+		return ResponseEntity.ok(new ArticolDTO(true, result));
+	}
+	
+	@GetMapping("/alldomains")
+	public ResponseEntity<ArticolDTO> getDomains() {
+		List<Articol> articles = this.articolService.getAllArticles();
+		List<String> result = articles.stream()
+				.map(Articol::getCategorie)
+				.collect(Collectors.toList());
+		if (result.isEmpty()) {
+			return ResponseEntity.ok(new ArticolDTO(false, result));
+		}
+		return ResponseEntity.ok(new ArticolDTO(true, result));
+	}
+	
+	@GetMapping("/articole-domeniu/{domeniu}")
+	public ResponseEntity<ArticolDTO> getArticlesByDomain(@PathVariable String domeniu) {
+		List<Articol> articles = this.articolService.getAllArticles();
+		List<Articol> result = articles.stream()
+				.filter(article -> article.getCategorie().equals(domeniu))
+				.collect(Collectors.toList());
+		if (result.isEmpty()) {
+			return ResponseEntity.ok(new ArticolDTO(false, result));
+		}
+		return ResponseEntity.ok(new ArticolDTO(true, result));
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<ArticolDTO> createArticle(@RequestBody Articol article) {
+		Object response = this.articolService.createArticle(article);
+		return ResponseEntity.ok(new ArticolDTO(true, response));
+	}
+	
+	@PostMapping("/update-articol")
+	public ResponseEntity<ArticolDTO> updateArticle(@RequestBody Articol article) {
+		Object response = this.articolService.updateArticol(article);
+		return ResponseEntity.ok(new ArticolDTO(true, response));
+	}
+}
--- src/main/java/com/example/demo/service/ArticolService.java
+++ src/main/java/com/example/demo/service/ArticolService.java
@@ -3,7 +3,6 @@ package com.example.demo.service;
 import com.example.demo.model.Articol;
 import com.example.demo.model.User;
 import com.example.demo.repository.ArticolRepository;
-import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,8 +20,19 @@ public class ArticolService {
         this.articolRepository = articolRepository;
         this.userService = userService;
     }
-
-    public void createAndInsertArticol(String userEmail, String category, String title, String content, String version) {
+    
+    public Object createArticle(Articol article) {
+        // Add any additional logic or validation if needed
+        String userEmail = article.getAutor().getEmail(); // Assuming you have this method
+        String category = article.getCategorie();
+        String title = article.getTitlu();
+        String content = article.getContinut();
+        String version = "1.0"; // Replace with the actual version
+        
+        return createAndInsertArticol(userEmail, category, title, content, version);
+    }
+    
+    public Object createAndInsertArticol(String userEmail, String category, String title, String content, String version) {
         try {
             Optional<User> optionalUser = userService.getUserByEmail(userEmail);
 
@@ -33,12 +43,55 @@ public class ArticolService {
                 articolRepository.save(articol);
             } else {
                 // Handle the case where the user with the specified email is not found.
-                throw new RuntimeException("User not found for email: " + userEmail);
+                return "User not found for email: " + userEmail;
             }
         } catch (Exception e) {
             // Handle the exception as per your application's requirements.
             // You may want to log the exception, return an error response, etc.
-            e.printStackTrace();
+            return e;
+        }
+        return "Article created successfully";
+    }
+    
+    public Object updateArticol(Articol article) {
+        try {
+            String userEmail = article.getAutor().getEmail(); // Assuming you have this method
+            String category = article.getCategorie();
+            String title = article.getTitlu();
+            String content = article.getContinut();
+            Float version = Float.valueOf(article.getVersiune());
+            Optional<Articol> optionalArticol = articolRepository.findById(article.getId());
+            
+            if (optionalArticol.isPresent()) {
+                Articol existingArticol = optionalArticol.get();
+                Optional<User> optionalUser = userService.getUserByEmail(userEmail);
+                User existingUser;
+                if (optionalUser.isPresent()) {
+                    existingUser = optionalUser.get();
+                } else {
+                    // Handle the case where the user with the specified email is not found.
+                    return "User not found for email: " + userEmail;
+                }
+                // Update the properties of the existing article
+                existingArticol.setAutor(existingUser);
+                existingArticol.setCategorie(category);
+                existingArticol.setTitlu(title);
+                existingArticol.setContinut(content);
+                existingArticol.setDataUltimeiModificari(new Date());
+                existingArticol.setVersiune(String.valueOf(version + 1));
+                
+                // Save the updated article back to the repository
+                articolRepository.save(existingArticol);
+                
+                return "Article updated successfully";
+            } else {
+                // Handle the case where the article with the specified ID is not found.
+                return "Article not found for ID: " + article.getId();
+            }
+        } catch (Exception e) {
+            // Handle the exception as per your application's requirements.
+            // You may want to log the exception, return an error response, etc.
+            return e;
         }
     }
 
@@ -49,7 +102,6 @@ public class ArticolService {
     public Articol getArticolByTitle(String title) {
         return articolRepository.findByTitlu(title);
     }
-
 
     private Articol createArticol(User user, String category, String title, String content, String version) {
         Articol articol = new Articol();
