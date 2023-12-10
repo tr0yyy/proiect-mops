c975837dd820b2b23e85e9bbf187b2967a6b42c8
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2023-12-10 02:52:52 +0200

    fix: cors for register and login

---
+++ src/main/java/com/example/demo/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/core/register")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+        registry.addMapping("/core/login")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
